683dddc71a6c58cbca768e0d971c23dfee25892c
Xiao-lin Yang <lemon2mc@163.com>
2022-12-27 21:14:35 +0800

    Warpeddisk code added. 2022-12-27.

--- examples/warped_disk/src/main.c
+++ examples/warped_disk/src/main.c
@@ -47,49 +47,78 @@ double Fp( ptcl *pt, double p, double n3, double rin, double rout,
 }
 
 
-void disk( ptcl * p, double mudisk, double rdisk_out )
+void warpeddisk( ptcl * p, double mudisk, double rdisk_out )
 { 
 	double beta, alpha;
-	double deltax, deltay, rms1, pem, bomiga, ut_em;
+	double deltax, deltay, pem, bomiga, ut_em;
 	double somiga_em, expnu_em, exppsi_em, expmu1_em, expmu2_em;
 	double r_em; //, mu_em, phi_em, time_em, sigma_em, sign_pr, sign_pth;
 	double g;
-	int m;
+	int m, caserange, bisection;
 
 	metricg( p );
-	rms1 = rms( p->a_spin );  /* rms gives the radius of the 
-				   * ISCO (inner most stable circular orbits) */
-	m = 1200;
+
 	// (111) in Yang & Wang (2012).
-	//this->velocity_ini[1] = this->robs/(robs**(three/two)+a_spin)*zero;
-	//this->velocity_ini[2] = this->robs/(robs**(three/two)+a_spin)*cos(45.D0*dtors)*zero;
-	//this->velocity_ini[3] = this->robs/(robs**(three/two)+a_spin)*sin(45.D0*dtors)*zero;
+	//velocity(1)=-0.D0!expmu1_obs/expnu_obs*robs/(robs**(three/two)+a_spin)!*zero
+	//velocity(2)=0.D0!expmu2_obs/expnu_obs/(robs**(three/two)+a_spin)*cos(45.D0*dtor)!*zero
+	//velocity(3)=0.D0!exppsi_obs/expnu_obs*(one/(robs**(three/two)+a_spin)*sin(45.D0*dtor)-somiga_obs)!*zero   
 
 	center_of_image( p );
 	printf( " beta c = %f \t alphac = %f \n", p->betac, p->alphac );
 
-	deltax = 80.0 / m;
-	deltay = 30.0 / m;
-	//fopen(unit=15,file='tdiskg.txt',status="replace");
-	FILE *fp = fopen("./plot/diskg5.txt", "w"); 
+	m = 400;
+	deltax = 110.0 / m;
+	deltay = 110.0 / m;
+
+
+	double rin, rout, gama0, n1, n2, n3;
+	n1 = 4.0;
+	n2 = 4.0;
+	n3 = 0.950; 
+	rin = rms( p->a_spin ); /* rms gives the radius of the 
+				* ISCO (inner most stable circular orbits) */
+	rout = 50.0;
+	// caserange=4, means muup, mudown and phy1, phy2 are not provided.
+	caserange = 4;
+	// parameters to describe the curved surface of warped disk and sended to
+	// function Fp as dummy variable.
+
+	bisection = false;
+
+	FILE *fp = fopen("./plot/warpdiskg.txt", "w"); 
 
 
 	//for ( int i = 330; i <= 330; i++ ) {
 	for ( int i = 0; i <= m; i++ ) {
-		beta = p->betac-i*deltay + 15.0;
+		beta = p->betac-i*deltay + 55.0;
 		Set_beta( p, beta );
 		//for ( int j = 448; j <= 449; j++ ) {
 		for ( int j = 0; j <= m; j++ ) {
-			alpha = p->alphac - j * deltax + 40.0;
+			alpha = p->alphac - j * deltax + 55.0;
 			Set_alpha( p, alpha );
 			lambdaq( p );
-			// call pemdisk just gives the direct image.      
-			//pem = Pemdisk( p, mudisk, rdisk_out, rms1, &r_em );
-			// while call pemdisk_all will gives the direct and high-order images.
+
 			pem = Pemdisk_all( p, mudisk, rdisk_out, rms1, &r_em );
+			//pem = pemfind(f1234,lambda,q,sinobs,muobs,a_spin,robs,scal,&                              
+                        //rin,rout,muup,mudown,phy1,phy2,caserange,Fp,paras,bisection)   
 
 			if ( pem != -one && pem != -two ) {
-				r_em = radius( p, pem );
+				YNOGKC( pt, pem );
+				sinp=sqrt(one-mua**two)  
+				//write(unit=6,fmt=*)re,mua,phya
+				beta=n3*sin(PI/two*(re-rin)/(rout-rin))
+				gamma0=paras(4)*dtor+paras(5)*PI*exp(paras(6)*(rin-re)/(rout-rin)) 
+
+				phy0=atan(tan(phya-gamma0)*cos(beta))
+				sin_phy0=sign(sin(phy0),sin(phya-gamma0))
+				cos_phy0=sign(cos(phy0),cos(phya-gamma0))
+				V=re/(re**(three/two)+a_spin)
+				V_theta=V*(-cos(phya-gamma0)*cos(beta)*sin_phy0*mua+sin(phya-gamma0)*cos_phy0*mua&
+							-sin(beta)*sin_phy0*sinp)
+				V_phi=V*(sin(phya-gamma0)*cos(beta)*sin_phy0+cos(phya-gamma0)*cos_phy0) 
+
+				theta_dot=V_theta/re
+				phi_dot=V_phi/re/sinp
 
 				//YNOGK( p, pem, &r_em, &mu_em, &phi_em, 
 				//	&time_em, &sigma_em, &sign_pr, &sign_pth ); 
@@ -144,7 +173,7 @@ int main( int argc, char *argv[])
 	//photon_ctor( &pt, a_spin, rini, muobs, sinobs, scal );
 	particle_construct( pt, a_spin, rini, muobs, sinobs, scal, Vobs );
 
-	disk( pt, zero, 30.0 );
+	warpeddisk( pt, zero, 30.0 );
 
 	free(pt);
 
